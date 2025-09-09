039bb0de6c713a946770fbd690f7208c5807fcbf
Prince Kibali M <princekibali222@gmail.com>
2025-09-09 07:01:04 +0300

    modifies index.js

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+// multiply: should equal 62
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+// random: should generate a random integer > 0
+const random = Math.floor(Math.random() * 1000) + 1;
+
+// mod: remainder should equal 4
+const num3 = 24;
+const num4 = 5;
+const mod = num3 % num4;
+
+// max: should return 20 as the highest number in the set
+const max = Math.max(1, 2, 3, 20);
